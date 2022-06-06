8bed7c42a948fb93f246995b51c844d7a7762233
Eduardo Marques <eduardo.marques@olist.com>
2022-06-06 14:40:00 -0300

    Change props name to be more declarative

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 type Input = {
   space: number[];
-  segmentsNumber: number;
+  interval: number;
 };
 
-export const getBiggerValue = ({ space, segmentsNumber }: Input) => {
+export const getBiggerValue = ({ space, interval }: Input) => {
   let minValues: number[] = [];
-  let invervalEnd = segmentsNumber;
+  let invervalEnd = interval;
 
   for (let intervalBegin = 0; intervalBegin < space.length; intervalBegin++) {
     if (invervalEnd <= space.length) minValues = [...minValues, Math.min(...space.slice(intervalBegin, invervalEnd++))];
